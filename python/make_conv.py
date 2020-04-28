f7749775bcf48eeeb2efdecca19234fb731cb2d1
ndbeste <ndbeste@gmail.com>
2020-04-28 02:05:58 -0700

    fix conv2

--- python/make_conv.py
+++ python/make_conv.py
@@ -28,7 +28,7 @@ module conv2\n\
     (\n\
     input  logic [0:18*12*12 -1]      image         ,\n\
     input  logic [0:18*60*5*5-1]      kernels       ,\n\
-    output logic [0:18*60*24*24*bW-1] xor_out \n\
+    output logic [0:18*60*8*8*bW-1] xor_out \n\
     );\n\
 ")
 
@@ -37,7 +37,7 @@ f.write("\n")
 
 for i in range(18):
     for j in range(60):
-        f.write("convchan2 c_2_%s (.i_image(image[%s*12*12:%s*12*12-1]), .i_kernel(kernels[%s*5*5:%s*5*5-1]), .o_out_fmap(xor_out[%s*24*24*bW:%s*24*24*bW-1]));\n"%(i,i,i+1,i*j,i*j+1,i*j,i*j+1))
+        f.write("convchan2 c_2_%s (.i_image(image[%s*12*12:%s*12*12-1]), .i_kernel(kernels[%s*5*5:%s*5*5-1]), .o_out_fmap(xor_out[%s*8*8*bW:%s*8*8*bW-1]));\n"%(i,i,i+1,i*j,i*j+1,i*j,i*j+1))
 
 f.write("\nendmodule")
 f.close()
